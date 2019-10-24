0d5b51850006070fb8ddb8459470ff904ccb4349
Antonio <Memphis2Coder@gmail.com>
2019-10-23 22:08:51 -0500

    test_2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Test from "./components/Test/Test";
+import Test_2 from "./components/Test_2/Test_2";
 
 import "./App.css";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Test />
+      <Test_2 />
     </div>
   );
 }
